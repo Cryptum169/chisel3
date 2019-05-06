0fc3871c3c1685328429691e414b3f0a4965df30
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2019-05-06 15:49:56 -0400

    [wip] Add PRNG and LFSR
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

---
+++ src/main/scala/chisel3/util/random/Prng.scala
@@ -0,0 +1,12 @@
+// See LICENSE for license details.
+
+package chisel3.util.random
+
+import chisel3._
+import chisel3.util._
+
+class PrngIo(n: Int) extends Bundle {
+  val seed = Input(Valid(UInt(n.W)))
+  val y = Output(UInt(n.W))
+  override def cloneType: this.type = new PrngIo(n).asInstanceOf[this.type]
+}
